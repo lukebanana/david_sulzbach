cd71ab66be18344e98b398c61444400389781a9c
Lukas Abegg <Luki@gmx.ch>
2014-03-26 14:53:32 +0100

    JS Optimizing

--- sites/all/themes/dave/js/custom.js
+++ sites/all/themes/dave/js/custom.js
@@ -1,7 +1,6 @@
 var $ = jQuery.noConflict();
 var deviceWidth = $(window).width();
 var mobileDeviceWidth = 720;
-var navigationBarVisible = 1;
 
 // Find out top offsets for scrolling function
 var homePos;
@@ -11,42 +10,44 @@ var referPos;
 var berufsPos;
 var kontaktPos;
 
+
+
+// Changes active class of Navigation Links
+function changeMenuActiveClass(activeClass){
+  $('.nav-link').removeClass('selected').addClass('not-selected');
+  $(activeClass).removeClass('not-selected').addClass('selected');
+}
+
 // Check current position and corresponding active menu class
 function checkActiveMenuClass(currentPosition){
 
   // Find out top offsets for scrolling function
-  homePos = $(".home-row").offset().top;
-  aboutPos = $(".about-row").offset().top;
-  hoerPos = $(".hoerbeispiele-row").offset().top;
-  referPos = $(".references-row").offset().top;
-  berufsPos = $(".berufsdetails-row").offset().top;
-  kontaktPos = $(".kontakt-row").offset().top;
+  homePos = $('.home-row').offset().top;
+  aboutPos = $('.about-row').offset().top;
+  hoerPos = $('.hoerbeispiele-row').offset().top;
+  referPos = $('.references-row').offset().top;
+  berufsPos = $('.berufsdetails-row').offset().top;
+  kontaktPos = $('.kontakt-row').offset().top;
 
   // change active classes
   if(currentPosition >= homePos && currentPosition <= aboutPos){
-    changeMenuActiveClass("#home-link");
+    changeMenuActiveClass('#home-link');
   }else if(currentPosition >= aboutPos && currentPosition <= hoerPos){
-    changeMenuActiveClass("#about-link");
+    changeMenuActiveClass('#about-link');
   }else if(currentPosition >= hoerPos && currentPosition <= referPos){
-    changeMenuActiveClass("#hoerbeispiele-link");
+    changeMenuActiveClass('#hoerbeispiele-link');
   }else if(currentPosition >= referPos && currentPosition <= berufsPos){
-    changeMenuActiveClass("#references-link");
+    changeMenuActiveClass('#references-link');
   }else if(currentPosition >= berufsPos && currentPosition <= kontaktPos){
-    changeMenuActiveClass("#berufsdetails-link");
+    changeMenuActiveClass('#berufsdetails-link');
   }else if(currentPosition >= kontaktPos){
-    changeMenuActiveClass("#kontakt-link");
+    changeMenuActiveClass('#kontakt-link');
   }
-}
-
-// Changes active class of Navigation Links
-function changeMenuActiveClass(activeClass){
-  $(".nav-link").removeClass("selected").addClass("not-selected");
-  $(activeClass).removeClass("not-selected").addClass("selected");
 }
 
 // Sets Height of Flexslider Container
 function setContainerHeight(){
-  $(".home-row .flexslider_views_slideshow_slide, .home-row").css("height", $(".home-row .views-field-field-slider-bild").css("height"));
+  $('.home-row .flexslider_views_slideshow_slide, .home-row').css('height', $('.home-row .views-field-field-slider-bild').css('height'));
 }
 
 // Toggles visibility of Navigation Bar (CSS)
@@ -89,26 +90,26 @@ $(window).load(function(){
   // Set height for container after all images are loaded
   setContainerHeight();
 
-  var headerWrapperHeight = $("#header-wrapper").height();
+  var headerWrapperHeight = $('#header-wrapper').height();
 
   // Find out top offsets for scrolling function
-  homePos = document.getElementsByClassName("home-row").offsetTop;
-  aboutPos = document.getElementsByClassName("about-row").offsetTop;
-  hoerPos = document.getElementsByClassName("hoerbeispiele-row").offsetTop;
-  referPos = document.getElementsByClassName("references-row").offsetTop;
-  berufsPos = document.getElementsByClassName("berufsdetails-row").offsetTop;
-  kontaktPos = document.getElementsByClassName("kontakt-row").offsetTop;
+  homePos = document.getElementsByClassName('home-row').offsetTop;
+  aboutPos = document.getElementsByClassName('about-row').offsetTop;
+  hoerPos = document.getElementsByClassName('hoerbeispiele-row').offsetTop;
+  referPos = document.getElementsByClassName('references-row').offsetTop;
+  berufsPos = document.getElementsByClassName('berufsdetails-row').offsetTop;
+  kontaktPos = document.getElementsByClassName('kontakt-row').offsetTop;
 
   // Array with link IDs and matching row classes
-  var array = [["#home-link", ".home-row"],
-              ["#about-link", ".about-row"],
-              ["#hoerbeispiele-link", ".hoerbeispiele-row"],
-              ["#references-link", ".references-row"],
-              ["#berufsdetails-link", ".berufsdetails-row"],
-              ["#kontakt-link", ".kontakt-row"]
+  var array = [['#home-link', '.home-row'],
+              ['#about-link', '.about-row'],
+              ['#hoerbeispiele-link', '.hoerbeispiele-row'],
+              ['#references-link', '.references-row'],
+              ['#berufsdetails-link', '.berufsdetails-row'],
+              ['#kontakt-link', '.kontakt-row']
             ];
   // Set first menu entry as selected when page loads
-  $(".nav-link").first().addClass("selected");
+  $('.nav-link').first().addClass('selected');
 
 
   // Create Click Function for mobile menu link
@@ -122,9 +123,10 @@ $(window).load(function(){
       $(entry[0]).click(function() {
         if (deviceWidth <= mobileDeviceWidth) {
           $('#navigation-bar').slideToggle('slow/400/fast');
-        };
+        }
+
         // Scroll to position of row, minus size of the header (overlapping)
-        $("html, body").animate({
+        $('html, body').animate({
           scrollTop: ($(entry[1]).offset().top - headerWrapperHeight + 10)
         }, 800, function(){
           $('html,body').clearQueue();
@@ -137,7 +139,7 @@ $(window).load(function(){
   $(window).scroll(function() {
     var currentPosition = 0;
     currentPosition= window.pageYOffset;
-    currentPosition += $("#header-wrapper").height();
+    currentPosition += $('#header-wrapper').height();
 
     checkActiveMenuClass(currentPosition);
   });
@@ -146,7 +148,7 @@ $(window).load(function(){
   $('body').bind('touchmove', function(e) {
     var currentPosition = 0;
     currentPosition= window.pageYOffset;
-    currentPosition += $("#header-wrapper").height();
+    currentPosition += $('#header-wrapper').height();
 
     checkActiveMenuClass(currentPosition);
   });
